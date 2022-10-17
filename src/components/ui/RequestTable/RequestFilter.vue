2df15eecc1649990d1c9a6cde74d7f5b6bfdcc52
poli8512 <poli8512@gmail.com>
2022-10-17 14:02:10 +0300

    feat double sort

--- src/components/ui/RequestTable/RequestFilter.vue
+++ src/components/ui/RequestTable/RequestFilter.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h6>Сортировка</h6>
-        <input type="text" v-model="sortInputByName" @input="sortItems"/>
+        <input type="text" v-model="sortInputByName" @input="changeInput"/>
         <div>
             <app-select v-model:status="sortByStatus" @changeSelect="changeSelect" title="по статусу">
                 <option value="all">Показать все</option>
@@ -24,50 +24,42 @@
 			const store = useStore()
 			const sortInputByName = ref('')
 			const sortByStatus = ref('')
-			const optionsData = store.getters['/request/getOptionsData']
 			const requests = store.getters['request/getRequests']
-          const onSortByFio=(string,regString)=>{
 
-          }
-          const onSortByStatus=()=>{
-
-          }
-			const sortItems = (e) => {
-				const filteredRequests = requests.filter(el => {
-					if (new RegExp(e.target.value).test(el.fio)) {
-						if (sortByStatus.value && el.status === sortByStatus.value) {
+			const onSortByFio = (dataRequest, regString) => {
+				if (regString) {
+					return dataRequest.filter(el => {
+						if (new RegExp(regString).test(el.fio)) {
 							return el
-						}else if (!sortByStatus.value){
-							return el
-                        }
-					}
-				})
-				emit('onFilter', filteredRequests)
+						}
+					})
+				}
+				return dataRequest
 			}
-
+			const onSortByStatus = (dataRequest) => {
+				if (sortByStatus.value === 'all') {
+					return dataRequest
+				}
+				if (sortByStatus.value) {
+					return dataRequest.filter(el => el.status === sortByStatus.value)
+				}
+				return dataRequest
+			}
+			const sortItems = () => {
+				const filteredRequests = onSortByFio(requests, sortInputByName.value)
+				const doubleResult = onSortByStatus(filteredRequests)
+				emit('onFilter', doubleResult)
+			}
+			const changeInput = (e) => {
+				sortInputByName.value = e.target.value
+				sortItems()
+			}
 			const changeSelect = (data) => {
-				let reqSorts = []
-				if (data === 'all') {
-					if (sortInputByName.value) {
-						sortItems(sortInputByName.value)
-					}
-					else {
-						const ar = requests.map(el => el)
-						emit('onFilter', ar)
-					}
-					return
-				}
-				if (sortInputByName.value) {
-					reqSorts = props.requests.filter(req => req.status === data)
-				}
-				else {
-					reqSorts = requests.filter(req => req.status === data)
-				}
-
-				emit('onSortStatus', reqSorts)
+				sortByStatus.value = data
+				sortItems()
 			}
 			return {
-				optionsData, sortInputByName, sortItems, sortByStatus, changeSelect
+				sortInputByName, sortByStatus, changeSelect, changeInput
 			}
 		}
 	}
